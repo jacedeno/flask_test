8f42b0af217ceca88f1a1cb04ff71bb550505509
Jose Cedeno <joseangel.cedeno@gmail.com>
2023-05-06 08:21:04 -0500

    modified application to use basic Jinja templates

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index ():
-    return "hello world"
+def index():
+    return render_template('index.html', pageTitle='My Calculator')
 
 @app.route('/jose')
 def jose():
-    return "hello Jose!"
+    return render_template('jose.html', pageTitle='About Jose Cedeno')
 
 if __name__ == '__main__':
     app.run (debug=True)
